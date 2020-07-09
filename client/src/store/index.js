f1d0569b6f0f0176f52bf619c5a2f0fbd0a64756
Brylle Mutia <bryllemutia3@gmail.com>
2020-07-09 14:04:42 +0800

    removed redux devtools before production

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
 import thunk from "redux-thunk";
 
 const middleware = [thunk];
 
 const store = createStore(
-    rootReducer,
-    compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    rootReducer, applyMiddleware(...middleware)
+    // compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;
